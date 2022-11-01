af086bc64e3317ea15ecf3e497ef8fc221b815be
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2022-11-01 00:43:43 -0400

    test labels changed, test coverage command added to package.json

--- __tests__/sciname.test.ts
+++ __tests__/sciname.test.ts
@@ -3,9 +3,9 @@ import request from "supertest";
 
 import app from "../app";
 
-describe("Get /", () => {
+describe("sciname metadata", () => {
 
-    describe('sciname metadata', () => {
+    describe('GET /', () => {
         it("should return all scinames", async () => {
             const scinameVar = 'common_name';
             const res = await request(app).get("/sciname").send({variable: scinameVar});
@@ -19,5 +19,11 @@ describe("Get /", () => {
             expect(res.status).toBe(200);
             expect(res.body[scinameVar].length).toBeGreaterThan(0);
           });
+
+          it("should error when request is malformed", async() => {
+            const scinameVar = 'sciname';
+            const res = await request(app).get("/sciname").send({variable: 1});
+            expect(res.status).toBe(400);
+          })
     });
 });
