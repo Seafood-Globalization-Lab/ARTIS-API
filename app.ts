7fe64a977f33a71ac2163741ffcf479347480ec2
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2022-11-10 16:26:38 -0500

    production metadata table available path

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import scinameRouter from './routers/sciname'
 import countriesRouter from './routers/countries'
+import productionRouter from './routers/production'
 import snetRouter from './routers/snet'
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use('/sciname', scinameRouter);
 app.use('/countries', countriesRouter);
+app.use('/production', productionRouter);
 app.use('/snet', snetRouter);
 
 export default app;
--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,8 @@
 
+export * from './connect_db';
 export * from './sciname';
 export * from './countries';
-export * from './connect_db';
+export * from './production';
+export * from './products';
+export * from './baci';
 export * from './snet';
