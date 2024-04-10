ca81fef9fb3b3ddfbd0488b692f115f5d09d40ea
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2024-04-10 16:40:05 -0400

    API key authentication works with timestamp and unlimited timestamp expiration for super users

--- middleware/authenticate.ts
+++ middleware/authenticate.ts
@@ -9,14 +9,35 @@ export const authenticate_key = async (req, res, next) => {
     // Check api key against list of potential api keys
     const auth_record = await db_check_api_key(api_key);
 
-    if (auth_record.length == 1) {
+    if (auth_record.length === 1) {
         // API key is valid, pass request on to further checks or processing
-        next()
+
+        // Check API key expiration date
+        const expiration : string = auth_record[0]["expiration_date"];
+        if (expiration === "never") {
+            // If no expiration date has been issued ie master users or ARTIS website pass request along
+            next()
+        } else {
+            // check expiration date against today's date
+            const current_timestamp = new Date().getTime();
+            const expiration_timestamp = new Date(expiration).getTime();
+
+            // if expiration timestamp is after current timestamp process request
+            if (expiration_timestamp > current_timestamp) {
+                next()
+            }
+            else {
+                // HTTP 403 response code: Access denied (eg. Invalid API key)
+                return res.status(403).json({ "message": "Invalid API key" });
+            }
+        }
     }
     else {
         // HTTP 403 response code: Access denied (eg. Invalid API key)
         return res.status(403).json({ "message": "Invalid API key" });
     }
+
+    
 }
 
 // Registers a new API user and returns their new API key
