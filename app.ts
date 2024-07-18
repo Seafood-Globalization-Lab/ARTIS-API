9613ea0b3e8b14b3fcfb0ee8d7be8f369d9dda77
Dan Martens <dan@friendsoftheweb.com>
2024-07-18 14:51:44 -0400

    Remove converted routes

--- app.ts
+++ app.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import accessRouter from './routers/authenticate';
-import supplementalRouter from './routers/supplemental';
-import snetRouter from './routers/snet';
 import consumptionRouter from './routers/consumption';
 
 const app = express();
@@ -9,10 +7,6 @@ const app = express();
 app.use(express.json());
 // creates, deletes, alters user permissions
 app.use('/access', accessRouter);
-// manages all requests for metadata
-app.use('/supplemental', supplementalRouter);
-// manages all requests for seafood trade network data
-app.use('/snet', snetRouter);
 // manages all requests for consumption data
 app.use('/consumption', consumptionRouter);
 
